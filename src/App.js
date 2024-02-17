a4725616c3a6ade4a7ebe168fdaa0dabebdd78f1
sunbang123 <musicianksy@naver.com>
2024-02-17 13:32:20 +0900

    #0 movie-app을 gitpages 페이지로 만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      // `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json`
+    ).then((response) => response.json())
+    .then((json) => console.log(json));
+  }, [])
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
