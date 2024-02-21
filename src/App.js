aa2ed41fe2320f7bdcaddc85970ccaeac145a0da
sunbang123 <musicianksy@naver.com>
2024-02-21 16:59:45 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 
+COOKIE_SET = cookiesecret // .env 파일에서 키 = 값 형식으로 환경변수들 저장
+
 function App() {
   const [loading, setLoading] = useState(true);
   // const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      // `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json`
+      // `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.COOKIE_SECRET}`
     ).then((response) => response.json())
     .then((json) => console.log(json));
   }, [])
